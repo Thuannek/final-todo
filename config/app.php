71d682c5a2a2478f9dd993252ad306a489f25bbd
Thuannek <thuan.le210219@vnuk.edu.vn>
2023-12-20 17:41:45 +0700

    Create Telescope

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
         App\Providers\VoltServiceProvider::class,
     ])->toArray(),
 
@@ -185,5 +186,7 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
+    
+    
 
 ];
